ab0eb8f8666fe1195c26de3a0e018778ebad2604
ffidolen <idolenkov@gmail.com>
2025-09-28 09:46:25 +0300

    2

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -5,6 +5,7 @@ from http import HTTPStatus
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
@@ -20,4 +21,3 @@ class TaskiAPITestCase(TestCase):
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
         self.assertTrue(models.Task.objects.filter(title='Test').exists())
-        
